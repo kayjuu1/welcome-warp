eece6c9f1ee99903fa1e7d4911be69093917c888
kayjuu1 <kwawusu1@gmail.com>
2024-04-26 17:22:14 +0000

    Added error message to the Login form

--- main.js
+++ main.js
@@ -1,28 +1,21 @@
-// navbar
-const loginBtn = document.querySelector(".loginBtn");
-const signUpBtn = document.querySelector(".signUpBtn");
-
-loginBtn.addEventListener("click", () => {
-    window.location.href = "loginPage.html";
-});
-
-signUpBtn.addEventListener("click", () => {
-    window.location.href = "loginPage.html";
-});
-
 // loginPage
-const emailInput = document.querySelector('#floatingInput');
-const password = document.querySelector('#floatingPassword');
-const login = document.querySelector("#loginBtn");
-const msg = document.querySelector('.msg');
 
+const login = document.querySelector("#loginBtn");
 login.addEventListener("click", function(e) {
     e.preventDefault();
+
+    const emailInput = document.querySelector('#floatingInput');
+    const password = document.querySelector('#floatingPassword');
+
+    const msg = document.querySelector('.msg');
+
     if (emailInput.value === "" || password.value === "") {
         msg.classList.add('error');
         msg.innerHTML = 'please enter all fields';
+        msg.style.color = 'red';
     } else {
+        msg.classList.remove('error');
+        msg.innerHTML = '';
         alert("Success");
     }
 });
-console.log(emailInput.value, password.value)
